b8cd8f16d41887d3a0f121a648e89247057661f4
Дмитрий Чехонадских <112265709+loseex@users.noreply.github.com>
2025-04-22 16:20:55 +0500

    Added provider to compose function

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,4 +1,8 @@
 import compose from "compose-function";
 import { withQueryClient } from "./with-query-client";
+import { withReactRouter } from "./with-react-router";
 
-export const withProviders = compose<React.FC>(withQueryClient);
+export const withProviders = compose<React.FC>(
+  withQueryClient,
+  withReactRouter
+);
